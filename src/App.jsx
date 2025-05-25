cd54e027c11623a1d8e6c545acafb1da4cb05532
muhammad-js <rabomuhammad60@gmail.com>
2025-05-25 11:24:48 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* ---------------------------- 2. src/App.jsx ----------------------------- */
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
